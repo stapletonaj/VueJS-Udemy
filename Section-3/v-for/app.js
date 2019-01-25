059fc4c23c9984c1caf4bc39a9246c42c50849c2
stapletonaj <stapleton.aj@gmail.com>
2019-01-25 17:41:14 +1030

    section 3 start

---
+++ Section-3/v-for/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data:{
+        ingredients: ['meat', 'fish', 'vegetables'],
+        persons: [
+            {name: 'Andy', age: 33, color: 'blue'},
+            {name: 'Kate', age: 'unknown', color: 'Turquoise'},
+    ]
+    }
+})
