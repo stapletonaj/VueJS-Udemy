858f364d2605406b5435cde0b5799eb26b049e00
Andrew Stapleton <stapleton.aj@gmail.com>
2019-02-05 17:14:32 +1030

    Up to compoents exercise

---
+++ Section-7/Section Code (Start)/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Components</h1>
+                <p> {{status}} </p>
+            </div>
+        </div>
+    <app-servers></app-servers>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Section-7/Section Code (Start)/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <hr>
+        <p>Server = {{ status }}</p>
+        <button v-on:click="changeStatus">Change Status</button>
+        <br>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            status: 'critical'
+        }
+    },
+    methods: {
+        changeStatus: function(){
+            this.status = "Normal"
+        }
+    }
+}
+</script>
