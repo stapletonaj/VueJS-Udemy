25deb4fbb00acb2996e225240ca45a2355baf6b4
Andrew Stapleton <stapleton.aj@gmail.com>
2019-02-12 17:27:09 +1030

    props passing exercise

---
+++ Exercises/Passing props/Start/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <app-server-details
+                :server = "server"
+                @serverDetailsChanged = "updateDetails()">
+            </app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header,
+            servers: Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        },
+        data: function() {
+            return {server: {id: 0, status: "Normal"}}
+        },
+        methods:{
+            updateDetails(e) {
+                console.log(e);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Exercises/Passing props/Start/src/components/Server/Servers.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    v-for="server in servers"
+                    @click = "serverClicked(server)">
+                Server #{{ server.id }},
+                Status = {{ server.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Critical'},
+                    {id: 3, status: 'Unknown'},
+                    {id: 4, status: 'Normal'},
+                ]
+            }
+        },
+        methods: {
+            serverClicked(server){
+                this.$emit("serverDetails", server);
+                
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
