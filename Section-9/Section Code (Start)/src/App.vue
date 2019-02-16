f20d4c1a5962d8f37f29017739f4dd5a627792e3
Andrew Stapleton <stapleton.aj@gmail.com>
2019-02-16 14:53:04 +1030

    beginning of section 9

---
+++ Section-9/Section Code (Start)/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <!-- We can use "slots" to pass data to a child component -->
+               <app-quote>
+                   <h1>{{ quoteTitle }}</h1>
+                   <p>What a wonderful Quote</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './assets/components/Quote.vue'
+
+    export default {
+        data: function() {
+            return {
+                quoteTitle: "A Quote",
+                quote : "Such a Wonderful Quote"
+                }
+        },
+        components: {
+            "app-quote": Quote, 
+        },
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ Section-9/Section Code (Start)/src/assets/components/Quote.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- This is a way to pass data from a prent using slots -->
+        <!-- The styling of the child applies to the slot i.e put it here NOT in the parent-->
+        <slot></slot>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: ['quote'],
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
