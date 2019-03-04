6c969ac8e1b272e392fc510f5462a5617252e27a
Andrew Stapleton <stapleton.aj@gmail.com>
2019-03-04 20:27:29 +1030

    dynamic components

--- Section-9/Section Code (Start)/src/App.vue
+++ Section-9/Section Code (Start)/src/App.vue
@@ -2,28 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
+                <button @click="selectedComponent = 'app-quote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+               <!--  is keyword is used to dynamically show components like below: -->
+                <keep-alive>
+                    <component :is="selectedComponent"></component>
+                </keep-alive>
                <!-- We can use "slots" to pass data to a child component -->
-               <app-quote>
+               <!-- <app-quote>
                    <h1 slot="title">{{ quoteTitle }}</h1>
                    <p slot="content">What a wonderful Quote</p>
-               </app-quote>
+               </app-quote> -->
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from './assets/components/Quote.vue'
+    import Quote from './assets/components/Quote.vue';
+    import Author from './assets/components/Author.vue';
+    import New from './assets/components/New.vue';
 
     export default {
         data: function() {
             return {
                 quoteTitle: "A Quote",
-                quote : "Such a Wonderful Quote"
+                quote : "Such a Wonderful Quote",
+                selectedComponent: "app-quote"
                 }
         },
         components: {
             "app-quote": Quote, 
+            appAuthor: Author,
+            appNew: New,
         },
         
     }
